b14ca6558ffad7e76ab6a030b8c9b9173cfe7339
stolaputih-amanzero <stolaputih-amanzero@users.noreply.github.com>
2026-07-06 12:38:17 +0700

    fix(config): add images.unsplash.com to remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,11 @@ const nextConfig: NextConfig = {
         hostname: "gxkbxuksdfaqfmrwyuno.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
     ],
   },
 };
